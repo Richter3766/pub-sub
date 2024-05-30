9dd16201d1b98f8609fb306d32e2aa47a0ab7ac3
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-05-30 15:34:17 +0900

    Env. 웹소켓 연결을 위한 설정

---
+++ src/lib/customAxios.js
@@ -0,0 +1,17 @@
+/** @format */
+import axios from "axios";
+
+const { REACT_APP_API_URL } = process.env;
+
+
+const customAxios = axios.create({
+  baseURL: REACT_APP_API_URL + "/api",
+});
+
+customAxios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+);
+
+export default customAxios;
